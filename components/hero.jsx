332bb5c198a886730662f1ef4d122a0f9d65e146
Sushant Kushwaha <sushantkushwaha289@gmail.com>
2025-04-08 23:55:05 +0530

    changes

--- components/hero.jsx
+++ components/hero.jsx
@@ -29,7 +29,7 @@ const HeroSection = () => {
   return (
     <section className="pt-40 pb-20 px-4">
       <div className="container mx-auto text-center">
-        <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
+        <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title bg-gradient-to-pl from-blue-500 to-blue-700 bg-clip-text font-bold">
           Manage Your Finances <br /> with Intelligence
         </h1>
         <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
